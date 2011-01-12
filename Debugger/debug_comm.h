209f5e7bdc2ded21191bbc39fb6756f37e4680cf
Tat-Chee Wan (USM) <tcwan@cs.usm.my>
2011-01-12 15:49:34 +0800

    implement usb message fragmentation and send logic

--- Debugger/debug_comm.h
+++ Debugger/debug_comm.h
@@ -27,7 +27,7 @@
 
 #ifdef __NXOS__
 
-  .extern       nx_usb_is_connected
+  .extern		nx_usb_is_connected
   .extern       nx_usb_can_write
   .extern       nx_usb_write
   .extern       nx_usb_data_written
